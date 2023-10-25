1e7774154955e2b90ffbe1311eac5c69cfe53401
Daniel Stanko <stankodanijel@gmail.com>
2023-10-25 08:38:01 +0200

    fix route

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 // app.use(fileUpload({}))
-app.use('/api', router)
+app.use('', router)
 
 app.use(errorHandler)
 
